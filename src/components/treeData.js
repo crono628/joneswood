f1fc8eca4bca49cc221a02e7e8a9b84827e5891a
crono628 <84817352+crono628@users.noreply.github.com>
2022-08-18 12:02:29 -0400

    firebase update

--- src/components/treeData.js
+++ src/components/treeData.js
@@ -2,9 +2,9 @@ export const treeData = [
   {
     tree: 'The White Oak Tree',
     description: [
-      'The white oak tree has graced the earth for centuries. Growing up to 100 feet tall, these lofty historians can live for hundreds of years. They grow on average approximately 12” per year and prefer full sunshine. ',
+      ' The white oak tree has graced the earth for centuries. Growing up to 100 feet tall, these lofty historians can live for hundreds of years. They grow on average approximately 12” per year and prefer full sunshine. ',
 
-      'The white oak also produces plentiful acorns every 4-10 years. Acorns are the fruit of the white oak that a large variety of animals include in their diet. From squirrels to bears, animals enjoy the white oaks generous contribution to dinner.',
+      ' The white oak also produces plentiful acorns every 4-10 years. Acorns are the fruit of the white oak that a large variety of animals include in their diet. From squirrels to bears, animals enjoy the white oaks generous contribution to dinner.',
 
       'The cellular structure of the white oak results in the woods supreme characteristic of making it water-resistant and rot-resistant. Furniture, finishings, construction, whiskey barrels and shipbuilding are just some of the roles that the white oak has a history of being suited for. ',
     ],
@@ -12,7 +12,7 @@ export const treeData = [
   {
     tree: 'The Red Oak Tree',
     description: [
-      'The red oak tree is a giant amongst the forest, growing up to 75 feet tall, a crown spread of 45 feet, and a trunk diameter over 3 feet wide. However, the trunk can diameter can reach over 6 feet wide if grown in the open. Growing upwards at more than 24” per year, this tree will continue to astound your eyes for years to come- up to 500! ',
+      'The red oak tree is a giant amongst the forest growing up to 75 feet tall with a crown spread of 45 feet and a trunk diameter over 3 feet wide. However, the trunk can diameter can reach over 6 feet wide if grown in the open. Growing upwards at more than 24” per year, this tree will continue to astound your eyes for years to come- up to 500! ',
 
       'Acorns from the red oak provide nutrition for forest dwelling animals and stay on throughout winter, unlike white oak trees. Oak trees rely on animals to disperse acorns since the acorn is quite heavy and won’t get blown very far by the wind. ',
 
