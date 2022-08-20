365a17f3e367e0562ee071ae6bd9334d2b1b2e8f
crono628 <84817352+crono628@users.noreply.github.com>
2022-08-19 22:07:53 -0400

    updated masonry to accept context

--- src/components/MasonryImages.js
+++ src/components/MasonryImages.js
@@ -4,26 +4,10 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { getDownloadURL, listAll } from 'firebase/storage';
 import { compressedRef } from '../firebase';
+import { useImgContext } from '../Context';
 
 const MasonryImages = () => {
-  const [images, setImages] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const getImages = async () => {
-      try {
-        const imageUrls = await listAll(compressedRef);
-        let mappedUrls = imageUrls.items.map((item) => getDownloadURL(item));
-        Promise.all(mappedUrls).then((thing) => {
-          setImages(thing);
-          setLoaded(true);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getImages();
-  }, []);
+  const { images, loading } = useImgContext();
 
   return (
     <div className="masonry-width-container">
@@ -32,16 +16,15 @@ const MasonryImages = () => {
         sx={{ overflowY: 'scroll', scrollbarWidth: 'none' }}
       >
         <ImageList variant="masonry" cols={3} gap={8}>
-          {loaded &&
-            images.map((item, index) => (
-              <ImageListItem key={item + index}>
-                <img
-                  src={`${item}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
+          {images.map((item, index) => (
+            <ImageListItem key={item + index}>
+              <img
+                src={`${item}?w=248&fit=crop&auto=format`}
+                srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
         </ImageList>
       </Box>
     </div>
